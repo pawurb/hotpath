f7d80193a3aa3e337d4b0699218af944890c9a58
Marc Espin <marc@mespin.me>
2025-09-12 21:21:17 +0200

    fix

--- crates/hotpath/src/lib.rs
+++ crates/hotpath/src/lib.rs
@@ -255,13 +255,13 @@ macro_rules! measure_block {
 #[macro_export]
 macro_rules! measure_block_cfg {
     ($label:expr, $expr:expr) => {{
+        #[cfg(feature = "hotpath")]
+        let __label_static: &'static str = $label;
+        #[cfg(feature = "hotpath")]
+        let __t0 = ::std::time::Instant::now();
         let __hotpath_out = $expr;
         #[cfg(feature = "hotpath")]
-        {
-            let __label_static: &'static str = $label;
-            let __t0 = ::std::time::Instant::now();
-            $crate::send_measurement(__label_static, __t0.elapsed());
-        }
+        $crate::send_measurement(__label_static, __t0.elapsed());
         __hotpath_out
     }};
 }
